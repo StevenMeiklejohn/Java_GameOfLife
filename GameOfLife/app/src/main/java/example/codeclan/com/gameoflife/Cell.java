c4f6755fc370c19ced68c9c010a41dbe9a08658c
StevenMeiklejohn <stevenmeiklejohn700@gmail.com>
2017-06-23 16:21:38 +0100

    Refactored code

--- GameOfLife/app/src/main/java/example/codeclan/com/gameoflife/Cell.java
+++ GameOfLife/app/src/main/java/example/codeclan/com/gameoflife/Cell.java
@@ -6,36 +6,68 @@ package example.codeclan.com.gameoflife;
 
 public class Cell {
 
+    static final
+
+    static final Cell DEAD = new Cell(false);
+    static final Cell ALIVE = new Cell(true);
+    static final int RESURECTION = 3;
+
+
     boolean status;
 
     public Cell(boolean status){
         this.status = status;
     }
 
+    @Override
+    public boolean equals(Object otherCell){
+        Cell cell = (Cell)otherCell;
+        return this.status == cell.status;
+    }
+
     public Cell tick(int numberOfNeighbours) {
         Cell next = new Cell(true);
         if(this.status) {
-            if (numberOfNeighbours <= 2 || numberOfNeighbours > 3) {
-                next.status = false;
-            } else {
-                next.status = true;
-            }
+            ifLiveCell(numberOfNeighbours, next);
             return next;
         }
        else{
-            if (numberOfNeighbours == 3){
-                next.status = true;
-            }
-            else {
-                next.status = false;
-            }
+            ifDeadCell(numberOfNeighbours, next);
             return next;
             }
         }
 
-
-
-    public boolean getStatus() {
-        return this.status;
+    private void ifDeadCell(int numberOfNeighbours, Cell next) {
+        if(ReproductionSuitability(numberOfNeighbours)){
+            next.status = true;
+        }
+        else {
+            next.status = false;
+        }
     }
+
+    private void ifLiveCell(int numberOfNeighbours, Cell next) {
+        if(Underpopulated(numberOfNeighbours) || Overcrowding(numberOfNeighbours)) {
+            next.status = false;
+        } else {
+            next.status = true;
+        }
+    }
+
+    private boolean ReproductionSuitability(int numberOfNeighbours) {
+        return numberOfNeighbours == 3;
+    }
+
+    private boolean Overcrowding(int numberOfNeighbours) {
+        return numberOfNeighbours > 3;
+    }
+
+    private boolean Underpopulated(int numberOfNeighbours) {
+        return numberOfNeighbours <= 2;
+    }
+
+
+//    public boolean getStatus() {
+//        return this.status;
+//    }
 }
--- GameOfLife/app/src/test/java/example/codeclan/com/gameoflife/CellTest.java
+++ GameOfLife/app/src/test/java/example/codeclan/com/gameoflife/CellTest.java
@@ -6,6 +6,8 @@ import android.telecom.Call;
 import org.junit.Test;
 
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+import static example.codeclan.com.gameoflife.Cell.ALIVE;
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
@@ -22,35 +24,36 @@ public class CellTest {
     public void DeadCell_WhenThreeNeighbours_AliveNextGen(){
         Cell deadCell = new Cell(true);
         Cell nextGeneration = deadCell.tick(3);
-        assertTrue(nextGeneration.getStatus());
+        assertEquals(Cell.ALIVE, nextGeneration);
 
     }
+
 
     @Test
     public void DeadCell_WhenTwoNeighbours_DeadInNextGen(){
         Cell deadCell = new Cell(true);
         Cell nextGeneration = deadCell.tick(2);
-        assertFalse(nextGeneration.getStatus());
+        assertEquals(Cell.DEAD, nextGeneration);
     }
-
+//
     @Test
     public void DeadCell_WhenLessThanTwoNeighbours_DeadInNextGen(){
         Cell deadCell = new Cell(true);
         Cell nextGeneration = deadCell.tick(1);
-        assertFalse(nextGeneration.getStatus());
+        assertEquals(Cell.DEAD, nextGeneration);
     }
-
+//
     @Test
     public void DeadCell_WhenThreeOrMoreNeighbours_DeadInNextGen(){
         Cell deadCell = new Cell(true);
         Cell nextGeneration = deadCell.tick(4);
-        assertFalse(nextGeneration.getStatus());
+        assertEquals(Cell.DEAD, nextGeneration);
     }
-
+//
     @Test
     public void DeadCell_WithExactlyThreeLiveNeighbours_AliveInNextGen(){
         Cell deadCell = new Cell(false);
         Cell nextGeneration = deadCell.tick(3);
-        assertTrue(nextGeneration.getStatus());
+        assertEquals(Cell.ALIVE, nextGeneration);
     }
 }
